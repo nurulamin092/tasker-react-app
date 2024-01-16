0b3e8ec5220c9472ab19905e17eef6eb9cc2a9ee
nurulamin092 <mnurulamin092@gmial.com>
2024-01-17 05:32:26 +0600

    headercomponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
+import Header from "./components/Header";
 function App() {
   return (
     <div className="bg-[#191D26] font-[Inter] text-white">
-      <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50">
-        <div className="container mx-auto flex items-center justify-between gap-x-6">
-          {/* <!-- Logo --> */}
-          <a href="/">
-            <img
-              className="h-[45px]"
-              src="./assets/lws-logo-en.svg"
-              alt="Lws"
-            />
-          </a>
-          {/* <!-- Logo Ends --> */}
-        </div>
-      </nav>
+      <Header />
       {/* <!-- Navbar Ends -->
 
 	<!-- Begin hero --> */}
