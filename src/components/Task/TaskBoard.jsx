439a72eb948ca322d8de61eefafbd4e501f2383d
nurulamin092 <mnurulamin092@gmial.com>
2024-01-17 18:51:52 +0600

    add modal and create new task

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -1,17 +1,37 @@
 import SearchTask from "./SearchTask";
 import TaskList from "./TaskList";
 import TaskActions from "./TaskActions";
+import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
+
 const TaskBoard = () => {
+  const defaultTasks = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTasks]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false);
+  }
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions />
-          <TaskList />
+          <TaskActions onAddClick={() => setShowAddModal(true)} />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
