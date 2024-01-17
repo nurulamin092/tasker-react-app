e5b4a77f7b9bf84076154825a0f37a90dde50318
nurulamin092 <mnurulamin092@gmial.com>
2024-01-17 20:45:33 +0600

    edit task

--- src/components/Task/AddTaskModal.jsx
+++ src/components/Task/AddTaskModal.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
-const AddTaskModal = ({ onSave }) => {
-  const [task, setTask] = useState({
-    id: crypto.randomUUID(),
-    title: "",
-    description: "",
-    tags: [],
-    priority: "",
-    isFavorite: false,
-  });
+const AddTaskModal = ({ onSave, taskToUpdate }) => {
+  const [task, setTask] = useState(
+    taskToUpdate || {
+      id: crypto.randomUUID(),
+      title: "",
+      description: "",
+      tags: [],
+      priority: "",
+      isFavorite: false,
+    }
+  );
 
+  const [isAdd, setIsAdd] = useState(Object.is(taskToUpdate, null));
   const handleChange = (evt) => {
     const name = evt.target.name;
     let value = evt.target.value;
@@ -29,7 +32,7 @@ const AddTaskModal = ({ onSave }) => {
        bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-1/4 left-1/3"
       >
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-          Add New Task
+          {isAdd ? "Add New Task" : "EditTask"}
         </h2>
 
         <div className="space-y-9 text-white lg:space-y-10">
@@ -97,7 +100,7 @@ const AddTaskModal = ({ onSave }) => {
             type="submit"
             className="rounded bg-blue-600 px-4 py-2
              text-white transition-all hover:opacity-80"
-            onClick={() => onSave(task)}
+            onClick={() => onSave(task, isAdd)}
           >
             Create new Task
           </button>
--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -16,22 +16,46 @@ const TaskBoard = () => {
   };
   const [tasks, setTasks] = useState([defaultTasks]);
   const [showAddModal, setShowAddModal] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null);
 
-  function handleAddTask(newTask) {
-    setTasks([...tasks, newTask]);
+  function handleAddTask(newTask, isAdd) {
+    if (isAdd) {
+      setTasks([...tasks, newTask]);
+    } else {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === newTask.id) {
+            return newTask;
+          } else {
+            return task;
+          }
+        })
+      );
+    }
+
     setShowAddModal(false);
+  }
+
+  function handleEditTask(task) {
+    setTaskToUpdate(task);
+    setShowAddModal(true);
   }
   return (
     <section className="mb-20" id="tasks">
-      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
+      {showAddModal && (
+        <AddTaskModal onSave={handleAddTask} taskToUpdate={taskToUpdate} />
+      )}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
 
-        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+        <div
+          className="rounded-xl border border-[rgba(206,206,206,0.12)]
+         bg-[#1D212B] px-6 py-8 md:px-9 md:py-16"
+        >
           <TaskActions onAddClick={() => setShowAddModal(true)} />
-          <TaskList tasks={tasks} />
+          <TaskList tasks={tasks} onEdit={handleEditTask} />
         </div>
       </div>
     </section>
